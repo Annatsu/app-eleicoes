9273ddd2727a689bb4d50f3a37d786183be8f39e
Brendon Pagano <brendonpagano@gmail.com>
2018-06-04 01:05:01 -0300

    Add a Phone simulator size component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import {
     Switch,
 } from 'react-router-dom';
 
+// Emotion
+import styled from 'react-emotion';
+
 
 // Application Routes
 import routes from './routes';
@@ -15,21 +18,33 @@ import routes from './routes';
 const APP_PREFIX = 'Eleições 2018';
 
 
+const HackyPhone = styled('div') `
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 375px;
+    height: 667px;
+`;
+
+
 const App = () => (
-    <Switch>
-        {
-            routes.map((r, i) => (
-                <Route
-                    key={r.id}
-                    path={r.url}
-                    render={() => {
-                        document.title = `${r.title} | ${APP_PREFIX}`;
-                        return r.component;
-                    }}
-                    exact={i === 0} />
-            ))
-        }
-    </Switch>
+    <HackyPhone>
+        <Switch>
+            {
+                routes.map((r, i) => (
+                    <Route
+                        key={r.id}
+                        path={r.url}
+                        render={() => {
+                            document.title = `${r.title} | ${APP_PREFIX}`;
+                            return r.component;
+                        }}
+                        exact={i === 0} />
+                ))
+            }
+        </Switch>
+    </HackyPhone>
 );
 
 
