9f28cba9f043fa522cbb8f6c6266e7eb8852a41a
Brendon Pagano <brendonpagano@gmail.com>
2018-06-04 00:23:59 -0300

    Re-write index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Mounts the App into the DOM node.
+ReactDOM.render(
+    <div/>,
+    document.getElementById('react-root')
+);
