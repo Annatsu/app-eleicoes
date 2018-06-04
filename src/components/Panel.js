3427c7fc8c9d3d7aafa1a8654af59dcc2f3107fc
Brendon Pagano <brendonpagano@gmail.com>
2018-06-04 02:06:07 -0300

    Add some stuff to Panel component

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -5,8 +5,13 @@ import React from 'react';
 import styled from 'react-emotion';
 
 
-const Container = styled('div') `
+const PanelContainer = styled('div') `
     background-color: #fff;
+    border-radius: 4px;
+    padding: 10px;
+    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+        0px 4px 5px 0px rgba(0, 0, 0, 0.14),
+        0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
 
 
@@ -14,19 +19,24 @@ const Panel = ({
     children,
     ...rest
 }) => (
-    <Container {...rest}>
-        {children}
-    </Container>
-);
+        <PanelContainer {...rest}>
+            {children}
+        </PanelContainer>
+    );
 
 
 export const PanelTitle = styled(({ children, className, ...rest }) => (
     <div className={`text-center ${className}`} {...rest}>
-        <h3>{children}</h3>
+        <h3 style={{ marginTop: '10px' }}>{children}</h3>
     </div>
 )) `
-    padding: 10px;
-    border-bottom: 1px solid black;
+    padding-bottom: 10px;
+    border-bottom: 1px solid rgba(0, 0, 0, .4);
+`;
+
+
+export const PanelBody = styled('div') `
+    padding-top: 10px;
 `;
 
 
