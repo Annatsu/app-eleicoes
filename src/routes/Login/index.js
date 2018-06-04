c68d09d0487816f87693e42b0d17be3d5268621a
Brendon Pagano <brendonpagano@gmail.com>
2018-06-04 01:09:26 -0300

    Add login index component

---
+++ src/routes/Login/index.js
@@ -0,0 +1,16 @@
+// React
+import React from 'react';
+
+
+const Login = () => (
+    <div className="container">
+        <div className="row">
+            <div className="col-xs-12">
+                <p>title</p>
+            </div>
+        </div>
+    </div>
+);
+
+
+export default Login;
